5960047923331ace3e9025cd534d723e657e6378
siddheshC <siddheshc.3mindsdigital@gmail.com>
2023-06-05 09:21:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Counter from './components/Counter';
 import { CounterProvider } from './context/CounterContext';
+import Demo from './components/Demo';
+import { NameProvider } from './context/NameContext';
+import User from './components/User';
 
 const App = () => {
   return (
     <>
       <CounterProvider>
         <Counter />
+        <Demo />
       </CounterProvider>
+
+      <NameProvider>
+        <User />
+      </NameProvider>
     </>
   );
 };
